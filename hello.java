62820afc26b0b795ee30008701181e3a311faef8
Deyanath <mohammaddeyanath2@gmail.com>
2025-02-03 15:54:13 +0530

    my first commit

---
+++ hello.java
@@ -0,0 +1,18 @@
+public class hello{
+    public static void main(String[] args) {
+        int a=6;
+        int b=2;
+        System.out.println(a<<b);
+        
+            
+    
+        
+    }
+    
+}
+
+
+    
+
+
+
