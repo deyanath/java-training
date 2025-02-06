a2154ed2c1c228631d2e6199c6244e252f5f100a
Deyanath <mohammaddeyanath2@gmail.com>
2025-02-06 12:03:17 +0530

    Continue

---
+++ day4/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        for(int i = 0;i<10;i++){
+            for(int j = 1;j <=10;j++)
+            //incomplete code
+            }
+        }
+    }
+}
---
+++ day4/Squareroot.java
@@ -0,0 +1,15 @@
+public class Squareroot {
+    public static void main(String[] args) {
+        int n=81;
+        int div=2;
+        while(div<=n/2)
+        {
+            if(n/div==div)
+            {
+                System.out.println(div);
+                break;
+            }
+            div++;
+        }
+    }
+}
